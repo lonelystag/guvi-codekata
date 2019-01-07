108620cb042e14e1f02ead3482d6e2ad2dc9b5a6
lonelystag <42843515+lonelystag@users.noreply.github.com>
2019-01-07 11:21:17 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,15 +1,16 @@
 N=int(input())
+sum=0
 print("Input:")
 print(N)
 if N<=100000:
-  sum = 0
-  temp = N
-  while temp > 0:
-   digit = temp % 10
-   sum += digit ** 3
-   temp //= 10
+  sum=0
+  temp=N
+  while temp>0:
+    digit=temp%10
+    sum+=digit**3
+    temp//=10
   print('Output:')
-  if N == sum:
-    print("Yes")
+  if N==sum:
+    print('yes')
   else:
-    print("No")
+    print('no')
