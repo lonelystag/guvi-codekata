ab48c47e421335f1e9a52bbcf46e75df1c7028cf
lonelystag <42843515+lonelystag@users.noreply.github.com>
2019-02-08 15:24:45 +0530

    Update armsinterval.py

--- armsinterval.py
+++ armsinterval.py
@@ -2,11 +2,11 @@ a=input().split()
 b=int(a[0])
 c=int(a[1])
 for num in range(b,c):
-  temp=num
+  temp=sum
   sum=0
   while temp>0:
-      digit=temp%10
-      sum=sum+digit**3
-      temp=temp//10
-      if sum==num:
-           print (num)
+    digit=temp%10
+    sum=sum+digit**3
+    temp=temp//10
+    if sum==num:
+      print(sum)
