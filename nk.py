6ff9ce616a5ca01303ba5b7d062dc58d445d5549
Ramesh S <42843515+lonelystag@users.noreply.github.com>
2019-02-20 21:10:50 +0530

    Update nk.py

--- nk.py
+++ nk.py
@@ -1,11 +1,8 @@
 sum=0
-a=[]
-n=int(input())
-k=int(input())
-for i in range(0,n):
-  m=input()
-  a.append(m)
-print(a)
-for j in range(0,k,1):
-  sum=(sum+a[j])
+a=input().split()
+b=input().split()
+n=int(a[0])
+k=int(a[1])
+for j in range(0,k):
+  sum=sum+int(b[j])
 print(sum)
